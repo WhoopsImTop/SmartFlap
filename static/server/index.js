23d6b45642352c0bf683856497f60052e2143c6e
elias <eliasenglen2@gmail.com>
2021-10-06 18:58:25 +0200

    add new

--- static/server/index.js
+++ static/server/index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const app = express()
 
-app.use('/uploads', express.static('/home/pi/Desktop/tfjs-customvision/static/pictures/'))
+app.use('/uploads', express.static('/home/pi/Desktop/SmartFlap/static/pictures/'))
 
 app.listen('3000', () => {
 	console.log("Listening")
